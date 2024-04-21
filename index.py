faa714eb5b3b21c91b4ec6817a14f8eebc0167bd
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2024-04-21 07:21:10 -0500

    index.py

--- index.py
+++ index.py
@@ -1,105 +1,237 @@
 import os
 import time 
 import random
-import multiprocessing as mp
 import threading as th
-def word_gen():
-    # Set the seed based on the current time
-
-    # List of characters to choose from for generating words
-    characters = 'abcdefghijklmnopqrstuvwxyz'
-
-    text = ''
-    for i in range (100):
-        random.seed(time.time())
-        for _ in range(1000):
-            # Generate a random word of length between 3 and 8 characters
-            word_length = random.randint(1, 12)
-            word = ''.join(random.choice(characters) for _ in range(word_length))
-            text += word + ' '
-        with open(f'text{i}.txt', 'a') as output:
-            output.write(text)
+from queue import Queue
+import filecmp
+# import multiprocessing as mp
+# from multiprocessing.pool import ThreadPool
 
 
-def delete_files():
-    for i in range(100):
-        try:
-            os.remove(f'text{i}.txt')
-        except FileNotFoundError:
+#verifica que la ruta sea un directorio
+def es_directorio(ruta):
+    return os.path.isdir(ruta)
+
+
+#serial version
+def serial_produccer( q:Queue, folder_path:str )->None:
+
+    with open(folder_path, 'r', encoding='utf-8') as output:
+        for line in output:
+            q.put(line)
+
+
+def serial_consumer(q:Queue, output_file:str)->None:
+    unique_words = []  
+    while not q.empty():
+        line = q.get()
+        tokens = line.split()
+        if tokens != []:
+            for token in tokens:
+                if token not in unique_words: 
+                    unique_words.append(token)  
+                else:
+                    pass
+    with open(output_file, 'a') as output:
+        output.write(str(unique_words))
+
+
+def serial(q:Queue, folder_path:str , output_file:str)->None:
+    files = os.listdir(folder_path)
+    txt_files = [file for file in files if file.endswith('.txt')]
+    if txt_files == []:
+        print("No .txt files in this directory")
+    else:
+        for file in txt_files:
+            file_path = os.path.join(folder_path, file)
+            serial_produccer(q,file_path)
+        serial_consumer(q, output_file)
+  
+
+#Threads
+def threads_producer(q:Queue, folder_path:str)->None:
+    # The producers read text from a collection of files, one per producer.
+    # They insert lines of text into a single shared queue
+    with open(folder_path, 'r', encoding='utf-8') as output:
+       for line in output:
+           q.put(line)
+
+
+def threads_consumer(q:Queue, output_file:str,unique_words:list,mutex:th.Lock )->None:
+    unique_words=[]
+    while not q.empty(): 
+        line = q.get()
+        tokens = line.split()
+        if tokens != []:
+            for token in tokens:
+                if token not in unique_words: 
+                    unique_words.append(token)  
+                else:
+                    pass
+                
+    with open(output_file, 'a') as output:
+        with mutex:
+            output.write(str(unique_words))
+
+
+def threadss(q:Queue,folder_path,output_file, mutex:th.Lock):
+    files = os.listdir(folder_path)
+    threads =[] 
+    txt_files = [file for file in files if file.endswith('.txt')]
+    unique_words = []
+    if txt_files == []:
+        print("No .txt files in this directory")
+    else:
+        for file in txt_files:
+            file_path = os.path.join(folder_path, file)
+            t = th.Thread(target=threads_producer, args=(q,file_path))
+            t.start()
+            threads.append(t)
+        for i in threads:
+            i.join()    
+        th.Thread(target=threads_consumer, args=(q,output_file,unique_words, mutex)).start()
+    
+#POOL
+"""No muestra mejorias con respecto al hecho con threads, es más, es significativamente más lento"""
+# def pool_producer(q:Queue, folder_path:str)->None:
+#     # The producers read text from a collection of files, one per producer.
+#     # They insert lines of text into a single shared queue
+#     with open(folder_path, 'r', encoding='utf-8') as output:
+#        for line in output:
+#            q.put(line)
+
+
+# def pool_consumer(q:Queue, output_file:str,unique_words:list,mutex:th.Lock )->None:
+#     unique_words=[]
+#     while not q.empty(): 
+#         line = q.get()
+#         tokens = line.split()
+#         if tokens != []:
+#             for token in tokens:
+#                 if token not in unique_words: 
+#                     unique_words.append(token)  
+#                 else:
+#                     pass
+                
+#     with open(output_file, 'a') as output:
+#         with mutex:
+#             output.write(str(unique_words))
+
+
+# def pool(q:Queue,folder_path:str,output_file:str, mutex:th.Lock)->None:
+#     files = os.listdir(folder_path)
+    
+#     unique_words = []
+#     txt_files = [file for file in files if file.endswith('.txt')]
+#     if txt_files == []:
+#         print("No .txt files in directory")
+#     else:
+#         with ThreadPool(os.cpu_count()//2) as pool:
+#             # start consumer tasks
+#             _ = [pool.apply_async(pool_producer, args=(q, file_path,)) for file_path in [os.path.join(folder_path, file) for file in txt_files]]
+#             # wait for all tasks to complete
+#             pool.close()
+#             pool.join()
+#         th.Thread(target=threads_consumer, args=(q,output_file,unique_words, mutex)).start()
+
+#Process
+
+"""La ejecuccion en procesos es muy lenta bajo las restricciones del problema, y la naturaleza de las 
+colas en multiprocessing"""
+# def process_producer(q:mp.Queue, folder_path:str)->None:
+#     # The producers read text from a collection of files, one per producer.
+#     # They insert lines of text into a single shared queue
+#     with open(folder_path, 'r', encoding='utf-8') as output:
+#        for line in output:
+#            q.put(line)
+
+
+# def process_consumer(q:mp.Queue, output_file:str,unique_words:list,mutex:mp.Lock )->None:
+#     unique_words=[]
+#     while not q.empty(): 
+#         line = q.get()
+#         tokens = line.split()
+#         if tokens != []:
+#             for token in tokens:
+#                 if token not in unique_words: 
+#                     unique_words.append(token)  
+#                 else:
+#                     pass
+                
+#     with open(output_file, 'a') as output:
+#         with mutex:
+#             output.write(str(unique_words)) 
+
+if __name__ == '__main__':
+    # Example usage
+    while True:
+        folder_path = input("Dijite un directorio con archivos .txt: ")
+        if es_directorio(folder_path) == True:
+            break
+        else:
+            print("El input digitado no es un directorio, por favor digite un directorio")
             pass
-    time.sleep(10)
+
+    mutex = th.Lock()
+
+    # folder_path = 'C:/Users/salce/Documents/universidad/Comp.paralelo/rp11/tests'  # RECUERDA PONER PARA QUE EL USUARIO COLOQUE EL PATH
+
+    #serial
+    output_file = 'Carlos_output.txt'
+    q = Queue()   
+    time1 = time.perf_counter()
+    serial(q,folder_path,output_file)
+    time2 = time.perf_counter()
+    print(f"serial time: {time2-time1}")
+
+    #Threads
+    output_file_t = 'Carlos_output_t.txt'
+    time_t1 = time.perf_counter()
+    threadss(q,folder_path,output_file_t,mutex)
+    time_t2 = time.perf_counter()
+    print(f"Threads time: {time_t2-time_t1}")
+
+    #pool
+    # output_file_tp = 'Carlos_output_tp.txt'
+    # time_tp1 = time.perf_counter()
+    # pool(q,folder_path,output_file_tp,mutex)
+    # time_tp2 = time.perf_counter()
+    # print(f"Threads Pool time: {time_tp2-time_tp1}")
+
+    #Process
+    # output_file_p = 'Carlos_output_p.txt'
+    # pmutex = mp.Lock()
+    # files = os.listdir(folder_path)
+    # txt_files = [file for file in files if file.endswith('.txt')]
+    # if txt_files == []:
+    #     print("No .txt files in directory")
+    # else:
+    #     ctx = mp.get_context('spawn')
+    #     unique_words = ctx.Manager().dict()
+    #     qp = ctx.Queue()
+    #     proceses =[]
+    #     time_p1 = time.perf_counter()
+    #     for file in txt_files:
+    #         file_path = os.path.join(folder_path, file)
+    #         p = ctx.Process(target=process_producer, args=(qp,file_path))
+    #         p.start()
+    #         proceses.append(p)
+
+    #     for i in proceses:
+    #         i.join()
+    #     p2 = ctx.Process(target=process_consumer, args=(qp,output_file_p,unique_words,pmutex)).start()
+    #     time_p2 = time.perf_counter()
+    # print(f"process time: {time_p2-time_p1}")  
+
+    #Comprobacion del contenido de los archivos output
+    time.sleep(1)
     try:
-        os.remove('hola.txt')
+        are_equals = filecmp.cmp('./'+output_file, './'+output_file_t)
+        if are_equals:
+            print(f"Los resultados entre {'./'+output_file} y {'./'+output_file_t} son iguales")
+        else:
+            print(f"Los resultados entre {'./'+output_file} y {'./'+output_file_t} son diferentes")
     except FileNotFoundError:
         pass
 
 
-#serial version
-def read_txt_files_in_folder(folder_path, output_file):
-    files = os.listdir(folder_path)
-    
-    txt_files = [file for file in files if file.endswith('.txt')]
-    
-    with open(output_file, 'a') as output:
-        for txt_file in txt_files:
-            file_path = os.path.join(folder_path, txt_file)
-            with open(file_path, 'r') as file:
-                words = file.read().split()
-                line = ''
-                for word in words:
-                    line += word + ' '
-                    # se define que una linea es un conjunto de 16 palabras
-                    if len(line.split()) == 16:
-                        output.write(line.strip() + '\n')
-                        line = ''
-                # si hay palabras remanentes las suba así. 
-                if line:
-                    output.write(line.strip() + '\n')
-
-#pararell version  
-              
-def producer(file_name, folder_path):
-    # The producers read text from a collection of files, one per producer.
-    # They insert lines of text into a single shared queue
-    txt_file_path = os.path.join(folder_path, file_name)
-    with open(txt_file_path, 'r', encoding='utf-8'):
-        
-    print()
-
-
-
-def consumer():
-    # The consumers take the lines of text and tokenize them. Tokens are “words” separated by white space. 
-    # When a consumer finds a token, it writes it to a file named
-    print()
-
-def process(file):
-    producer(file)
-    print()
-
-if __name__ == '__main__':
-    # Example usage
-    folder_path = '.'  # RECUERDA PONER PARA QUE EL USUARIO COLOQUE EL PATH
-    # word_gen()
-    output_file = 'hola.txt'
-    files = os.listdir(folder_path)
-    txt_files = [file for file in files if file.endswith('.txt')]
-
-    print(txt_files)
-    # time1 = time.perf_counter()
-    # read_txt_files_in_folder(folder_path, output_file)
-    # time2 = time.perf_counter()
-    # print(time2-time1)
-    ctx = mp.get_context('spawn')
-    result_dict = ctx.Manager().dict()
-    q = ctx.Queue()
-    proceses =[]
-    for file in txt_files:
-        p = ctx.Process(target=process, args=(file))
-        p.start()
-        proceses.append(p)
-
-    for i in proceses:
-        i.join()
-
-    # delete_files()
